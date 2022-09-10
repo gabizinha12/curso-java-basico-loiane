fc7f1fba2003842c3089379c5d7e2138396e8860
gabizinha12 <gabrielle.plima1965@gmail.com>
2022-09-10 13:22:00 -0300

    Aula 5 e 6

---
+++ javabasicoloiane/src/aula05/OlaMundo.java
@@ -0,0 +1,8 @@
+package aula05;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!");
+	}
+}
---
+++ javabasicoloiane/src/aula06/Argumentos.java
@@ -0,0 +1,8 @@
+package aula06;
+
+class Argumentos {
+	public static void main(String[] args) {
+		System.out.println("Você digitou " + args[0]);
+	}
+}
+// java é compilado e interpretado
