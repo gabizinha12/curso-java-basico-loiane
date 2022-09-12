eb755cacbbf913b61bad49a319decf7d1ba82c4d
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-09-12 13:17:02 -0300

    Aula 7

---
+++ javabasicoloiane/src/aula07/Erro02.java
@@ -0,0 +1,7 @@
+package aula07;
+class Erro02 { // nome da classe sempre em maiúsculo
+	 public static void main(String[] args) { // método de entrada da classe
+		int cont;
+		 System.out.println("Hello World");
+	 }
+ } 
---
+++ javabasicoloiane/src/aula07/Erro03.java
@@ -0,0 +1,7 @@
+package aula07;
+class Erro03 { // nome da classe sempre em maiúsculo
+	 public static void main(String[] args) { // método de entrada da classe
+		int cont;
+		 System.out.println("Você digitou  " + 1/0);
+	 }
+ } 
