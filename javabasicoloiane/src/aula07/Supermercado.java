3338665f221e847a3b07e93b8fefaafbcd7e37f1
gabizinha12 <gabrielle.plima1965@gmail.com>
2022-09-12 18:08:16 -0300

    Aula de Variáveis

---
+++ javabasicoloiane/src/aula07/Supermercado.java
@@ -0,0 +1,32 @@
+package aula07;
+
+import java.util.Scanner;
+
+public class Supermercado {
+	public static void main(String[] args) {
+		String[] frutasPromocao = { "banana", "maçã", "abacaxi", "melão", "mamão" };
+		System.out.println("Qual fruta você deseja comprar hoje?");
+		Scanner sc = new Scanner(System.in);
+		String escolhaFruta = sc.nextLine();
+
+		switch (escolhaFruta) {
+		case "maçã":
+		case "abacaxi":
+		case "melão":
+		case "mamão":
+		case "banana":
+			for(String frutas : frutasPromocao) {
+				frutas.contains(escolhaFruta);
+			}
+            System.out.println("Ótima escolha!Essa fruta está em promoção hoje.");
+			break;
+		case "default":
+			System.out.println("Fruta inválida");
+			break;
+            
+       
+		}
+		sc.close();
+	}
+
+}
---
+++ javabasicoloiane/src/aula08/Variaveis.java
@@ -0,0 +1,34 @@
+package aula08;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		
+		// convenção Java
+		int idade = 21;
+		String nome= "Gabizinha";
+		String nomeDoMeuCachorro = "Duke";
+		String ano2014 = "2014";
+
+		
+		
+		// aceito mas não utilizado
+		int _idade;
+		int $idade;
+		
+		// não é convenção Java
+		
+		String nome_do_meu_cachorro;
+		String NomeDoMeuCachorro;
+		String nomeDoMeucachorro;
+		
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		
+		// má prática
+		int a = 10;
+		String  b = "Loiane";
+		
+	}
+
+}
