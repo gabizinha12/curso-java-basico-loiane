e3d1d67a5ef3e104bb1f098756988a49d90af0a7
gabizinha12 <gabrielle.plima1965@gmail.com>
2022-09-13 18:00:36 -0300

    Aula 13 Operadores

--- javabasicoloiane/src/com/loiane/javabasico/aula07/ListaSupermercado.java
+++ javabasicoloiane/src/com/loiane/javabasico/aula07/ListaSupermercado.java
@@ -5,26 +5,28 @@ import java.util.Scanner;
 
 public class ListaSupermercado {
 	public static void main(String[] args) {
-		String[] frutasEmPromocao = {"banana", "maçã", "abacaxi", "melão", "mamão"};
+		String[] frutasEmPromocao = { "banana", "maçã", "abacaxi", "melão", "mamão" };
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Qual a sua lista de compras para hoje?");
 		String[] listaFrutas = new String[4];
 		int numeroFrutas = 0;
 		String minhasFrutas = "";
-		
-		
-		for(int i = 0; i < frutasEmPromocao.length; i++) {
-		//	System.out.println(frutasEmPromocao[i]);
-			for(int j = 0; j < listaFrutas.length; j++) {
-				minhasFrutas = sc.nextLine();
-				listaFrutas[i] += numeroFrutas;
-				//int desconto = (numeroFrutas * 5);
-				System.out.println(Arrays.toString(listaFrutas));
-				System.out.println(minhasFrutas);
-				//System.out.println(desconto);
-			//	if(frutasEmPromocao.equals(listaFrutas)) {
-					
-				//}
+
+
+
+		for (int i = 0; i < frutasEmPromocao.length; i++) {
+			for (int j = 0; j < listaFrutas.length; j++) {
+				if (minhasFrutas.equals(frutasEmPromocao)) {
+					minhasFrutas = sc.next();
+					numeroFrutas++;
+					listaFrutas[j] = minhasFrutas;
+					int desconto = (numeroFrutas * 5);
+					System.out.println("Você tem direito a %d de desconto" + desconto);
+					System.out.println("O %s está em promoção hoje." + minhasFrutas);
+					System.out.println(Arrays.toString(listaFrutas));
+					System.out.println(minhasFrutas);
+					System.out.println(desconto);
+				}
 
 			}
 		}
---
+++ javabasicoloiane/src/com/loiane/javabasico/aula13/OperadoresAritmeticos.java
@@ -0,0 +1,52 @@
+package com.loiane.javabasico.aula13;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		int resultado = 1 + 2;
+		System.out.println(resultado);
+
+		resultado = resultado - 1;
+		System.out.println(resultado);
+
+		resultado = resultado * 2;
+		System.out.println(resultado);
+
+		resultado = resultado / 2;
+		System.out.println(resultado);
+
+		resultado = resultado + 8;
+		System.out.println(resultado);
+
+		resultado = resultado % 7;
+		System.out.println(resultado);
+		
+		String primeiroNome = "Esta é";
+		String segundoNome = " uma String concatenada.";
+		String terceiroNome = primeiroNome + segundoNome;
+		System.out.println(terceiroNome);
+		
+		resultado = resultado + 1;
+		System.out.println(resultado);
+		
+		resultado++;
+		System.out.println(resultado++);
+		// mesma coisa que 
+		// System.out.println(resultado);
+		// resultado = resultado + 1;
+		// resultado += 1;
+		
+		System.out.println(++resultado);
+		// mesma coisa que 
+		// resultado = resultado + 1;
+		// System.out.println(resultado)
+		
+		
+		resultado--;
+		System.out.println(resultado);
+		
+		System.out.println(resultado--);
+		System.out.println(--resultado);
+	}
+
+}
