95ed313e0baa434839204dbe07a8f9deb71e817b
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-09-28 22:18:20 -0300

    Exercícios aula 11,12,13

---
+++ javabasicoloiane/src/com/loiane/javabasico/exercicios/Ex12.java
@@ -0,0 +1,16 @@
+package com.loiane.javabasico.exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex12 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);		
+		System.out.print("Digite sua altura:  ");
+		double altura = sc.nextDouble();
+		double pesoIdeal = (72.7 * altura) - 58;
+		System.out.printf("O seu peso ideal é :  %.2f", pesoIdeal);
+		sc.close();
+	}
+}
---
+++ javabasicoloiane/src/com/loiane/javabasico/exercicios/Ex13.java
@@ -0,0 +1,50 @@
+package com.loiane.javabasico.exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex13 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		System.out.println("Digite sua altura:  ");
+		double altura = sc.nextDouble();
+		System.out.println("Digite seu sexo:   ");
+		String sexo = sc.next();
+		System.out.println("Digite seu peso:  ");
+		double peso = sc.nextDouble();
+
+		double formula;		
+		switch(sexo.charAt(0)) {
+		case 'H':
+			formula = (72.7 * altura) - 58;
+		    System.out.printf("O seu peso ideal é : %.2f", formula);
+		    if(peso > formula) {
+				System.out.println(" Voce está acima do peso");
+				
+			} else if( peso < formula) {
+				System.out.println(" Voce esta abaixo do peso");
+			} else {
+				System.out.println(" Voce esta dentro do peso");
+			}
+		break;
+		case 'M':
+			formula = (62.1 * altura) - 44.7;
+		    System.out.printf("O seu peso ideal é : %.2f", formula);
+		    if(peso > formula) {
+				System.out.println(" Voce está acima do peso");
+				
+			} else if( peso < formula) {
+				System.out.println(" Voce esta abaixo do peso");
+			} else {
+				System.out.println(" Voce esta dentro do peso");
+			}
+		break;
+		default:
+			System.out.println("Sexo inválido");
+			break;	
+		}
+		
+		
+	}
+
+}
