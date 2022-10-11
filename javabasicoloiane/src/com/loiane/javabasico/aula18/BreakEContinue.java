adcd12f7190d87aec61396cdc3e54b3f1d4d8f34
gabrielle.pereira <gabrielle.pereira@7comm.com.br>
2022-10-11 18:01:34 -0300

    Aula 18

--- javabasicoloiane/src/com/loiane/javabasico/aula18/BreakEContinue.java
+++ javabasicoloiane/src/com/loiane/javabasico/aula18/BreakEContinue.java
@@ -5,20 +5,42 @@ import java.util.Scanner;
 public class BreakEContinue {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Entre com um número");
-		int num = sc.nextInt();
-		
-		System.out.println("Entre com um limite");
-		int max = sc.nextInt();
-		
-		for(int i = num; i <= max; i++) {
-			if(i % 7 == 0) {
-				System.out.println(i);
-				break;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Entre com um número");
+//		int num = sc.nextInt();
+//		
+//		System.out.println("Entre com um limite");
+//		int max = sc.nextInt();
+//		
+//		for(int i = num; i <= max; i++) {
+//			if(i % 7 == 0) {
+//				System.out.println(i);
+//				// break;
+//			}
+//		}
+
+		for (int i = 0; i <= 4; i++) {
+			rotulo1: {
+				rotulo2: {
+					rotulo3: {
+						if (i == 1) {
+							break rotulo1;
+						}
+						if (i == 2) {
+							break rotulo2;
+						}
+						if (i == 3) {
+							break rotulo3;
+						}
+						System.out.println("rotulo3");
+					}
+					System.out.println("rotulo2");
+				}
+				System.out.println("rotulo1");
 			}
+			System.out.println("Valor de i:  " + i);
+
 		}
 
 	}
-
 }
